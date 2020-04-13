5093338ba8f4891128a32030c7b3dbe9a6e72f74
Matthew Inamdar <matt@goldspecdigital.com>
2020-04-13 16:06:20 +0100

    Placed CFN template in seperate files

--- aws/outputs.py
+++ aws/outputs.py
@@ -1,41 +1,122 @@
-from troposphere import Output, Ref
+from troposphere import Output, Ref, GetAtt, Sub
 
 
-def get_bucket_name_output(template, bucket_name_variable):
+def get_database_name_output(template, database_username_variable):
     return template.add_output(
         Output(
-            'BucketName',
-            Description='The S3 bucket name',
-            Value=bucket_name_variable
+            'DatabaseName',
+            Description='The database name',
+            Value=database_username_variable
         )
     )
 
 
-def get_default_queue_output(template, default_queue_resource):
+def get_database_username_output(template, database_username_variable):
+    return template.add_output(
+        Output(
+            'DatabaseUsername',
+            Description='The username for the database',
+            Value=database_username_variable
+        )
+    )
+
+
+def get_database_host_output(template, database_resource):
+    return template.add_output(
+        Output(
+            'DatabaseHost',
+            Description='The host of the RDS instance',
+            Value=GetAtt(database_resource, 'Endpoint.Address')
+        )
+    )
+
+
+def get_database_port_output(template, database_resource):
+    return template.add_output(
+        Output(
+            'DatabasePort',
+            Description='The port of the RDS instance',
+            Value=GetAtt(database_resource, 'Endpoint.Port')
+        )
+    )
+
+
+def get_redis_host_output(template, redis_resource):
+    return template.add_output(
+        Output(
+            'RedisHost',
+            Description='The host of the Redis instance',
+            Value=GetAtt(redis_resource, 'RedisEndpoint.Address')
+        )
+    )
+
+
+def get_redis_port_output(template, redis_resource):
+    return template.add_output(
+        Output(
+            'RedisPort',
+            Description='The port of the Redis instance',
+            Value=GetAtt(redis_resource, 'RedisEndpoint.Port')
+        )
+    )
+
+
+def get_default_queue_output(template, default_queue_name_variable):
     return template.add_output(
         Output(
             'DefaultQueue',
-            Description='The URI of the default queue',
-            Value=Ref(default_queue_resource)
+            Description='The name of the default queue',
+            Value=default_queue_name_variable
         )
     )
 
 
-def get_notifications_queue_output(template, notifications_queue_resource):
+def get_notifications_queue_output(template, notifications_queue_name_variable):
     return template.add_output(
         Output(
             'NotificationsQueue',
-            Description='The URI of the notifications queue',
-            Value=Ref(notifications_queue_resource)
+            Description='The name of the notifications queue',
+            Value=notifications_queue_name_variable
         )
     )
 
 
-def get_search_queue_output(template, search_queue_resource):
+def get_load_balancer_domain_output(template, load_balancer_resource):
     return template.add_output(
         Output(
-            'SearchQueue',
-            Description='The URI of the search queue',
-            Value=Ref(search_queue_resource)
+            'LoadBalancerDomain',
+            Description='The domain name of the load balancer',
+            Value=GetAtt(load_balancer_resource, 'DNSName')
+        )
+    )
+
+
+def get_elasticsearch_host_output(template, elasticsearch_resource):
+    return template.add_output(
+        Output(
+            'ElasticsearchHost',
+            Description='The host of the Elasticsearch instance',
+            Value=GetAtt(elasticsearch_resource, 'DomainEndpoint')
+        )
+    )
+
+
+def get_docker_repository_uri_output(template, docker_repository_resource):
+    return template.add_output(
+        Output(
+            'DockerRepositoryUri',
+            Description='The URI of the Docker repository',
+            Value=Sub('${AWS::AccountId}.dkr.ecr.${AWS::Region}.amazonaws.com/${RepositoryName}',
+                      RepositoryName=Ref(docker_repository_resource))
+        )
+    )
+
+
+def get_docker_cluster_name_output(template, ecs_cluster_resource):
+    return template.add_output(
+        Output(
+            'DockerClusterName',
+            Description='The name of the Docker cluster',
+            Value=Ref(ecs_cluster_resource)
         )
     )
--- aws/variables.py
+++ aws/variables.py
@@ -1,25 +1,69 @@
 from troposphere import Join, Ref
 
 
-def get_bucket_name_variable(environment_parameter, uuid_parameter):
+def get_default_queue_name_variable(environment_parameter, uuid_parameter):
+    return Join('-', ['default', Ref(environment_parameter), Ref(uuid_parameter)])
+
+
+def get_notifications_queue_name_variable(environment_parameter, uuid_parameter):
+    return Join('-', ['notifications', Ref(environment_parameter), Ref(uuid_parameter)])
+
+
+def get_search_queue_name_variable(environment_parameter, uuid_parameter):
+    return Join('-', ['search', Ref(environment_parameter), Ref(uuid_parameter)])
+
+
+def get_uploads_bucket_name_variable(environment_parameter, uuid_parameter):
+    return Join('-', ['uploads', Ref(environment_parameter), Ref(uuid_parameter)])
+
+
+def get_api_launch_template_name_variable(environment_parameter):
+    return Join('-', ['api-launch-template', Ref(environment_parameter)])
+
+
+def get_docker_repository_name_variable(environment_parameter, uuid_parameter):
     return Join('-', ['api', Ref(environment_parameter), Ref(uuid_parameter)])
 
 
-def get_default_queue_name_variable(environment_parameter):
-    return Join('-', [Ref(environment_parameter), 'default'])
+def get_api_log_group_name_variable(environment_parameter):
+    return Join('-', ['api', Ref(environment_parameter)])
 
 
-def get_notifications_queue_name_variable(environment_parameter):
-    return Join('-', [Ref(environment_parameter), 'notifications'])
+def get_queue_worker_log_group_name_variable(environment_parameter):
+    return Join('-', ['queue-worker', Ref(environment_parameter)])
 
 
-def get_search_queue_name_variable(environment_parameter):
-    return Join('-', [Ref(environment_parameter), 'search'])
+def get_scheduler_log_group_name_variable(environment_parameter):
+    return Join('-', ['scheduler', Ref(environment_parameter)])
 
 
-def get_ci_user_name_variable(environment_parameter):
-    return Join('-', ['ci-api', Ref(environment_parameter)])
+def get_api_task_definition_family_variable(environment_parameter):
+    return Join('-', ['api', Ref(environment_parameter)])
+
+
+def get_queue_worker_task_definition_family_variable(environment_parameter):
+    return Join('-', ['queue-worker', Ref(environment_parameter)])
+
+
+def get_scheduler_task_definition_family_variable(environment_parameter):
+    return Join('-', ['scheduler', Ref(environment_parameter)])
 
 
 def get_api_user_name_variable(environment_parameter):
     return Join('-', ['api', Ref(environment_parameter)])
+
+
+def get_ci_user_name_variable(environment_parameter):
+    return Join('-', ['ci', Ref(environment_parameter)])
+
+
+def get_database_name_variable():
+    return 'the_leeds_repo'
+
+
+def get_database_username_variable():
+    return 'the_leeds_repo'
+
+
+def get_elasticsearch_domain_name_variable(environment_parameter):
+    return Join('-', ['search', Ref(environment_parameter)])
