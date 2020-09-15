2c29c7815b68cec2a914fec1eaefd942b0a8b51e
Matthew Inamdar <matt@goldspecdigital.com>
2020-09-15 12:07:42 +0100

    Hard coded password

--- database/factories/PendingOrganisationAdminsFactory.php
+++ database/factories/PendingOrganisationAdminsFactory.php
@@ -13,6 +13,6 @@ $factory->define(PendingOrganisationAdmin::class, function (Faker $faker) {
         'last_name' => $faker->lastName,
         'email' => $faker->safeEmail,
         'phone' => null,
-        'password' => bcrypt('secret'),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
     ];
 });
