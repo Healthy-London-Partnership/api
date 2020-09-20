03d83ad203da4693805dc72a17cb9f57ad84b2a3
Matthew Inamdar <matt@goldspecdigital.com>
2020-09-20 14:46:45 +0100

    Added location and social medias support for first follow ups command

--- app/Console/Commands/Hlp/Notify/OrganisationAdminInvitee/FirstFollowUpsCommand.php
+++ app/Console/Commands/Hlp/Notify/OrganisationAdminInvitee/FirstFollowUpsCommand.php
@@ -4,9 +4,12 @@ namespace App\Console\Commands\Hlp\Notify\OrganisationAdminInvitee;
 
 use App\Emails\OrganisationAdminInviteFirstFollowUps\NotifyInviteeEmail;
 use App\Generators\AdminUrlGenerator;
+use App\Models\Location;
 use App\Models\Notification;
 use App\Models\OrganisationAdminInvite;
+use App\Models\SocialMedia;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 
@@ -38,6 +41,7 @@ class FirstFollowUpsCommand extends Command
         }, range(1, 4));
 
         $organisationAdminInvites = OrganisationAdminInvite::query()
+            ->with('organisation', 'organisation.location', 'organisation.socialMedias')
             ->whereNotNull('email')
             ->whereIn(DB::raw('cast(`created_at` as date)'), $dates)
             ->get();
@@ -48,11 +52,15 @@ class FirstFollowUpsCommand extends Command
                     $organisationAdminInvite->email,
                     [
                         'ORGANISATION_NAME' => $organisationAdminInvite->organisation->name,
-                        'ORGANISATION_ADDRESS' => 'N/A', // TODO
+                        'ORGANISATION_ADDRESS' => $this->transformAddress(
+                            $organisationAdminInvite->organisation->location
+                        ) ?: 'N/A',
                         'ORGANISATION_URL' => $organisationAdminInvite->organisation->url ?: 'N/A',
                         'ORGANISATION_EMAIL' => $organisationAdminInvite->organisation->email ?: 'N/A',
                         'ORGANISATION_PHONE' => $organisationAdminInvite->organisation->phone ?: 'N/A',
-                        'ORGANISATION_SOCIAL_MEDIA' => 'N/A', // TODO
+                        'ORGANISATION_SOCIAL_MEDIA' => $this->transformSocialMedias(
+                            $organisationAdminInvite->organisation->socialMedias
+                        ) ?: 'N/A',
                         'ORGANISATION_DESCRIPTION' => $organisationAdminInvite->organisation->description,
                         'INVITE_URL' => $adminUrlGenerator->generateOrganisationAdminInviteUrl(
                             $organisationAdminInvite
@@ -61,5 +69,76 @@ class FirstFollowUpsCommand extends Command
                 )
             );
         }
+    }
+
+    /**
+     * @param \App\Models\Location|null $location
+     * @return string|null
+     */
+    protected function transformAddress(?Location $location): ?string
+    {
+        if ($location === null) {
+            return null;
+        }
+
+        $addressParts = [
+            $location->address_line_1,
+            $location->address_line_2,
+            $location->address_line_3,
+            $location->city,
+            $location->county,
+            $location->postcode,
+            $location->country,
+        ];
+
+        $addressParts = array_filter($addressParts);
+
+        return implode(', ', $addressParts);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Collection|null $socialMedias
+     * @return string|null
+     */
+    protected function transformSocialMedias(?Collection $socialMedias): ?string
+    {
+        if ($socialMedias === null) {
+            return null;
+        }
+
+        return $socialMedias
+            ->map(function (SocialMedia $socialMedia): string {
+                return $this->transformSocialMedia($socialMedia);
+            })
+            ->implode(', ');
+    }
+
+    /**
+     * @param \App\Models\SocialMedia $socialMedia
+     * @return string
+     */
+    protected function transformSocialMedia(SocialMedia $socialMedia): string
+    {
+        $type = $socialMedia->type;
+
+        switch ($type) {
+            case SocialMedia::TYPE_TWITTER:
+                $type = 'Twitter';
+                break;
+            case SocialMedia::TYPE_FACEBOOK:
+                $type = 'Facebook';
+                break;
+            case SocialMedia::TYPE_INSTAGRAM:
+                $type = 'Instagram';
+                break;
+            case SocialMedia::TYPE_YOUTUBE:
+                $type = 'YouTube';
+                break;
+            case SocialMedia::TYPE_OTHER:
+                $type = 'Other';
+                break;
+        }
+
+        return "{$type}: {$socialMedia->url}";
     }
 }
