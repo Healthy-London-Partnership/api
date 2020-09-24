da9490f2f8c2cdc62a17ada041ac7f8ac51cdff8
Matthew Inamdar <matt@goldspecdigital.com>
2020-09-24 14:56:58 +0100

    Removed class reference from migration

--- database/migrations/2020_09_09_151448_update_social_medias_add_polymorphic_fields.php
+++ database/migrations/2020_09_09_151448_update_social_medias_add_polymorphic_fields.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\UpdateRequest;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,7 +18,7 @@ class UpdateSocialMediasAddPolymorphicFields extends Migration
         });
 
         \DB::transaction(function () {
-            DB::update('update social_medias set sociable_id = service_id, sociable_type = ? where service_id is not null', [UpdateRequest::EXISTING_TYPE_SERVICE]);
+            DB::update('update social_medias set sociable_id = service_id, sociable_type = ? where service_id is not null', ['services']);
             Schema::table('social_medias', function (Blueprint $table) {
                 $table->dropForeign(['service_id']);
                 $table->dropColumn('service_id');
