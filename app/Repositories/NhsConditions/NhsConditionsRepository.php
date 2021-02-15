0aacd5e23981fb6852e87213581630aa3a7f240e
Matt Inamdar <matt@goldspecdigital.com>
2021-02-15 21:22:59 +0000

    Added controller

--- app/Repositories/NhsConditions/NhsConditionsRepository.php
+++ app/Repositories/NhsConditions/NhsConditionsRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repositories\NhsConditions;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use Psr\Http\Message\ResponseInterface;
 
 class NhsConditionsRepository
@@ -39,13 +40,17 @@ class NhsConditionsRepository
 
     public function find(string $slug): ResponseInterface
     {
-        return $this->client->get(
-            "{$this->domain}/conditions/{$slug}",
-            [
-                'headers' => $this->getHeaders(),
-                'timeout' => $this->timeout,
-            ]
-        );
+        try {
+            return $this->client->get(
+                "{$this->domain}/conditions/{$slug}",
+                [
+                    'headers' => $this->getHeaders(),
+                    'timeout' => $this->timeout,
+                ]
+            );
+        } catch (ClientException $exception) {
+            return $exception->getResponse();
+        }
     }
 
     protected function getHeaders(): array
